5e6cd374e99f76609ec23aa52ff94b49c1d91cdb
salgadotomas89 <salgadotomas@outlook.com>
2023-12-28 09:30:01 -0300

    obteniendo el margen del menu

---
+++ noticias/static/js/script.js
@@ -0,0 +1,50 @@
+(function () {
+    'use strict';
+
+    // Fetch the form we want to apply custom Bootstrap validation styles to
+    var form = document.getElementById('noticia-form');
+
+    // Prevent form submission if there are invalid fields
+    form.addEventListener('submit', function (event) {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+
+      form.classList.add('was-validated');
+    }, false);
+  })();
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const deleteLinks = document.querySelectorAll('.delete-link');
+
+    function deleteNoticia(id) {
+      $.ajax({
+        type: 'DELETE',
+        url: `/noticias/delete/${id}`,
+        headers: {
+          'X-CSRFToken': '{{ csrf_token }}',
+        },
+        success: function (data) {
+          const deletedNoticia = document.querySelector(`[data-id="${id}"]`);
+          if (deletedNoticia) {
+            deletedNoticia.closest('.col-md-6').remove();
+          }
+
+        },
+        error: function (error) {
+          console.error('Error al eliminar noticia:', error);
+        }
+      });
+    }
+
+    deleteLinks.forEach(link => {
+      link.addEventListener('click', function (event) {
+        event.preventDefault();
+        const id = this.getAttribute('data-id');
+        if (confirm('¿Estás seguro de que quieres eliminar esta noticia?')) {
+          deleteNoticia(id);
+        }
+      });
+    });
+  });
